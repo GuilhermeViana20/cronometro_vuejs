d87927266564ca0397100b455d50745478aa16e6
Guilherme - PC CASA <guilhermeviana347@gmail.com>
2021-07-14 23:43:30 -0300

    Finalizado escopo do projeto

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,70 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+
+    <img class="img" alt="Vue logo" src="./assets/cronometro.png">
+
+    <a class="timer" href="">00:00,00</a>
+
+    <div class="areaBtn">
+      <button class="botao">INICIAR</button>
+      <button class="botao">REINICIAR</button>
+    </div>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
 
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
+  name: 'App'
 }
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+  #app {
+    display: flex;
+    width: 100%;
+    align-items: center;
+    justify-content: center;
+    flex-direction: column;
+  }
+
+  .img {
+    width: 420px;
+    height: auto;
+    padding-top: 100px;
+  }
+
+  .timer {
+    color: #a435f0;
+    font-size: 70px;
+    margin-top: -250px;
+    text-decoration: none;
+  }
+
+  .areaBtn {
+    margin-top: 200px;
+  }
+
+  .botao {
+    -webkit-user-select: none;
+    -moz-user-select: none;
+    width: 150px;
+    background-color: #a435f0;
+    color: white;
+    font-weight: 700;
+    font-size: 20px;
+    border: 0;
+    border-radius: 5px;
+    text-align: center;
+    margin-left: 15px;
+    margin-right: 15px;
+    padding: 10px;
+    cursor: pointer;
+  }
+
+  .botao:hover {
+    opacity: 0.7;
+    transform: scale(1.1);
+    transition: all 300ms;
+  }
 </style>
